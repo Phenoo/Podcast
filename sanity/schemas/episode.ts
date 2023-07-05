7df60cbf16d6b9d3cfd9ff864fd74aa39883856b
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-05 05:46:22 +0000

    #fixing episode schema

--- sanity/schemas/episode.ts
+++ sanity/schemas/episode.ts
@@ -54,18 +54,14 @@ export default defineType ({
       type: 'text',
     },
     {  
+      
       name: 'content',
       title: 'Content',
-      type: 'blockContent',
-  
-    //  name: 'content',
-    //  title: 'Content',
-    //  description: 'An episode note. Where encoded is a string containing information about your episode.',
-    //  type: 'array',
-    //  of: [{
-    //   type: 'block'
-    //  }]
-     
+      description: 'An episode note. Where encoded is a string containing information about your episode.',
+      type: 'array',
+      of: [{
+       type: 'blockContent'
+      }]
     },
     {
       name: 'slug',
