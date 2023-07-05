542e43b320b544e3a2dc0ed84ae7afbca3bbdb50
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-05 05:15:55 +0000

    #EDIT

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -8,5 +8,5 @@ import blockContent from './schemas/blockContent'
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [podcast, episode, sponsor, category, blockContent]
+  types: [podcast, episode, sponsor, category, blockContent],
 }
