f771e0ebeec680f9cd6ab16821b15be870579188
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-13 06:15:40 +0000

    #updated episode schema. added blockcontent

--- sanity/schemas/episode.ts
+++ sanity/schemas/episode.ts
@@ -54,14 +54,17 @@ export default defineType ({
       type: 'text',
     },
     {  
-      
-      name: 'content',
-      title: 'Content',
-      description: 'An episode note. Where encoded is a string containing information about your episode.',
-      type: 'array',
-      of: [{
-       type: 'block'
-      }]
+     name: 'content',
+     title: 'Content',
+     type: 'blockContent'
+ 
+    //  name: 'content',
+    //  title: 'Content',
+    //  description: 'An episode note. Where encoded is a string containing information about your episode.',
+    //  type: 'array',
+    //  of: [{
+    //   type: 'block'
+    //  }]
     },
     {
       name: 'slug',
