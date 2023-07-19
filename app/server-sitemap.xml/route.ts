0904b4d105a09afa0a31a1a823a7a3ddb5e24955
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-19 04:23:47 +0000

    #edit

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -1,22 +1,32 @@
 // app/server-sitemap.xml/route.ts
-import { getServerSideSitemap } from 'next-sitemap'
+import { getServerSideSitemap } from 'next-sitemap';
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
 
 export async function GET(request: Request) {
   // Method to source urls from cms
-  // const urls = await fetch('https//example.com/api')
+    const query = groq`*[__type == "episode"]
+    {
+      slug
+    }`;
+  
+    const slugs = await client.fetch(query);
+    const slugRoutes = slugs.map((slug) => slug.slug.current);
 
-  return getServerSideSitemap([
-    {
-      loc: 'https://podcast-green-mu.vercel.app',
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-    {
-      loc: 'https://podcast-green-mu.vercel.app/dynamic-path-2',
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-  ])
-}
+    //const urls = await fetch('https://podcast-green-mu.vercel.app/api')
+
+    return getServerSideSitemap([
+        {
+        loc: 'https://podcast-green-mu.vercel.app',
+        lastmod: new Date().toISOString(),
+        // changefreq
+        // priority
+        },
+        {
+        loc: 'https://podcast-green-mu.vercel.app/dynamic-path-2',
+        lastmod: new Date().toISOString(),
+        // changefreq
+        // priority
+        },
+    ])
+    }
