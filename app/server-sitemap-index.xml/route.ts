8ce5654393739068d35a9721111b576463a4db47
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-21 03:15:34 +0000

    #edit

--- app/server-sitemap-index.xml/route.ts
+++ app/server-sitemap-index.xml/route.ts
@@ -1,35 +1,34 @@
-import { client } from "@/sanity/lib/client";
-import { groq } from "next-sanity";
-import { Page } from "@/app/type/types";
+ 
 // app/server-sitemap.xml/route.ts
-import { getServerSideSitemap } from 'next-sitemap'
- 
+import { getServerSideSitemap } from 'next-sitemap';
+import { client } from '@/sanity/lib/client';
+import { groq } from 'next-sanity';
+import { Page } from '@/app/type/types';
+
 export async function GET(request: Request) {
-  // Method to source urls from cms
-  //export async function getSourceUrlsFromCMS(): Promise<string[]> {
-    const query = groq`*[__type == "episode"] {
-      slug
-    }`;
-  
-    const slugs = await client.fetch<Page[]>(query);
-    const slugRoutes = slugs.map((slug) => slug.slug.current);
-  
-    //return slugRoutes;
-  
-  const urls = await fetch('https://podcast-green-mu.vercel.app/api')
- 
-  return getServerSideSitemap([
-    {
-      loc: 'https://podcast-green-mu.vercel.app',
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-    {
-      loc: 'https://podcast-green-mu.vercel.app/dynamic-path-2',
-      lastmod: new Date().toISOString(),
-      // changefreq
-      // priority
-    },
-  ])
+  // Method to source URLs from the CMS
+  const urls = await fetchUrlsFromCMS();
+
+  return getServerSideSitemap(urls);
+}
+
+async function fetchUrlsFromCMS(): Promise<{ loc: string; lastmod: string }[]> {
+  const query = groq`*[__type == "episode"] {
+    slug
+  }`;
+
+  const episodes = await client.fetch<Page[]>(query);
+
+  // You can customize the date format to match your needs
+  const currentIsoDate = new Date().toISOString();
+
+  // Map the episodes to URLs with lastmod as current ISO date
+  const urls = episodes.map((episode) => ({
+    loc: `https://podcast-green-mu.vercel.app/${episode.slug.current}`,
+    lastmod: currentIsoDate,
+    changefreq: 'daily', // Change frequency can be 'always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', or 'never'
+    priority: 0.8,       // Priority can be a value between 0.0 and 1.0, where 1.0 is the highest priority
+  }));
+
+  return urls;
 }
