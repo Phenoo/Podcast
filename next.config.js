5598e2bade1499bdb203a543cd630aab9891ebcf
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-21 04:47:24 +0000

    #updated nextconfig.js added with sitemap

--- next.config.js
+++ next.config.js
@@ -1,11 +1,23 @@
-/** @type {import('next').NextConfig} */
+const withSitemap = require('next-sitemap');
+
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   images: {
-    domains: ['images.pexels.com', "cdn.sanity.io", "images.unsplash.com", "w7.pngwing.com"]
-  }
-}
+    domains: ['images.pexels.com', 'cdn.sanity.io', 'images.unsplash.com', 'w7.pngwing.com'],
+  },
+  // Your other Next.js configuration options here
+};
 
-module.exports = nextConfig
+module.exports = withSitemap({
+  ...nextConfig, // Spread your existing nextConfig
+  // Options for next-sitemap
+  // You can customize these as per your project's requirements
+  // For example:
+  // exclude: ['/admin'],
+  // priorityMap: { '/': 1, '/dynamic-page': 0.8 },
+  // changefreq: 'daily',
+  // generateRobotsTxt: true,
+  // etc.
+});
