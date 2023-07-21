dab9c1260f778c21affe3b0447f4dee2b724d8bd
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-07-21 04:04:28 +0000

    #edit

--- app/server-sitemap-index.xml/route.ts
+++ app/server-sitemap-index.xml/route.ts
@@ -1,34 +1,50 @@
- 
 // app/server-sitemap.xml/route.ts
 import { getServerSideSitemap } from 'next-sitemap';
-import { client } from '@/sanity/lib/client';
-import { groq } from 'next-sanity';
+import groq from 'groq'; // Assuming you have the groq library for querying the CMS
+import {client} from '@/sanity/lib/client'; // Replace 'your-cms-client' with the actual client to interact with your CMS
 import { Page } from '@/app/type/types';
 
-export async function GET(request: Request) {
-  // Method to source URLs from the CMS
-  const urls = await fetchUrlsFromCMS();
-
-  return getServerSideSitemap(urls);
-}
-
-async function fetchUrlsFromCMS(): Promise<{ loc: string; lastmod: string }[]> {
+// Method to source static URLs from CMS using groq query
+export async function generateStaticParams() {
   const query = groq`*[__type == "episode"] {
     slug
   }`;
 
-  const episodes = await client.fetch<Page[]>(query);
+  const slugs = await client.fetch<Page[]>(query);
+  const slugRoutes = slugs.map((slug) => slug.slug.current);
 
-  // You can customize the date format to match your needs
-  const currentIsoDate = new Date().toISOString();
-
-  // Map the episodes to URLs with lastmod as current ISO date
-  const urls = episodes.map((episode) => ({
-    loc: `https://podcast-green-mu.vercel.app/${episode.slug.current}`,
-    lastmod: currentIsoDate,
-    changefreq: 'daily', // Change frequency can be 'always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', or 'never'
-    priority: 0.8,       // Priority can be a value between 0.0 and 1.0, where 1.0 is the highest priority
+  return slugRoutes.map((slug) => ({
+    loc: `https://podcast-green-mu.vercel.app/${slug}`, // Replace 'example.com' with your actual domain
+    lastmod: new Date().toISOString(),
+    // Add other optional properties like changefreq and priority here if needed
   }));
-
-  return urls;
 }
+
+export async function GET(request: Request) {
+  // Get the static URLs from the CMS
+  const staticUrls = await generateStaticParams();
+
+  // Combine the static and dynamic URLs for the sitemap
+  const sitemapUrls = [
+    {
+      loc: 'https://podcast-green-mu.vercel.app',
+      lastmod: new Date().toISOString(),
+      // Add other optional properties like changefreq and priority here if needed
+    },
+    ...staticUrls,
+  ];
+
+  // Generate the sitemap using next-sitemap's getServerSideSitemap function
+  const sitemap = await getServerSideSitemap(sitemapUrls);
+
+  // Set the response headers to indicate XML content type
+  const headers = new Headers();
+  headers.set('Content-Type', 'application/xml');
+
+  // Return the sitemap as a response
+  return new Response(sitemap, { headers });
+}
+
+
+
+
