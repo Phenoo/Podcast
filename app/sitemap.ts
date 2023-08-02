0b1b9ec51ff14a40ccaa8bf3f7f308d3f52fb6bd
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-08-02 23:32:46 +0000

    #edit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -54,31 +54,8 @@ export default async function sitemap() {
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 import { Page } from "@/app/type/types";
-import { getAllPosts } from "@/lib/getData";
-
-interface Category {
-  _id: string;
-  slug: string; // Update this to match your actual data structure
-}
-
-export async function getAllCategories(): Promise<Category[]> {
-  try {
-    // Define the GROQ query to fetch all categories from Sanity
-    const query = groq`*[_type == "category"] {
-      _id,
-      slug {
-        current
-      }
-    }`;
-
-    // Fetch the categories using the Sanity client
-    const categories = await client.fetch<Category[]>(query);
-    return categories;
-  } catch (error) {
-    console.error("Error fetching categories from Sanity:", error);
-    return [];
-  }
-}
+import { getAllPosts } from "@/lib/getData"; // Adjust the path based on your actual file structure
+import { getAllCategories } from "@/lib/getAllCategories"; // Adjust the path based on your actual file structure
 
 export default async function sitemap() {
   const baseUrl = "https://podcast-green-mu.vercel.app";
---
+++ lib/getAllCategories.ts
@@ -0,0 +1,26 @@
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
+
+interface Category {
+  _id: string;
+  slug: string; // Update this to match your actual data structure
+}
+
+export async function getAllCategories(): Promise<Category[]> {
+  try {
+    // Define the GROQ query to fetch all categories from Sanity
+    const query = groq`*[_type == "category"] {
+      _id,
+      slug {
+        current
+      }
+    }`;
+
+    // Fetch the categories using the Sanity client
+    const categories = await client.fetch<Category[]>(query);
+    return categories;
+  } catch (error) {
+    console.error("Error fetching categories from Sanity:", error);
+    return [];
+  }
+}
