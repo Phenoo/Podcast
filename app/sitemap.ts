e1ceed24b534cda01b890c7b39488568d19d4989
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-08-03 00:18:32 +0000

    #edit

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -54,22 +54,28 @@ export default async function sitemap() {
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 import { Page } from "@/app/type/types";
-import { getAllPosts } from "@/lib/getData"; // Adjust the path based on your actual file structure
-import { getAllCategories } from "@/lib/getAllCategories"; // Adjust the path based on your actual file structure
+import { getAllPosts } from "@/lib/getData";
+import { getAllCategories } from "@/lib/getAllCategories";
+
+interface Category {
+  _id: string;
+  slug: {
+    current: Slug; // Make sure the structure matches your schema definition
+  };
+}
 
 export default async function sitemap() {
   const baseUrl = "https://podcast-green-mu.vercel.app";
 
   // Get All Categories from CMS
   const categories = await getAllCategories();
+  console.log("Categories:", categories); // Add this line to inspect the structure
   const categoryUrls: { url: string; lastModified: Date }[] = [];
 
   for (const category of categories) {
-      categoryUrls.push({
-        url: `${baseUrl}/category/${category.slug.current}`,
-        lastModified: new Date(),
-      
-      
+    categoryUrls.push({
+      url: `${baseUrl}/category/${category.slug}`,
+      lastModified: new Date(),
     });
   }
 
