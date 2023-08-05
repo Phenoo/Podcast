bebfc3427ab9458e1d4b5287312f197fb972bfd0
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-08-05 08:22:14 +0000

    #feth metadata

--- app/episode/[slug]/page.tsx
+++ app/episode/[slug]/page.tsx
@@ -1,4 +1,4 @@
-
+{/*
 import { client } from "@/sanity/lib/client";
 import { groq } from "next-sanity";
 import { cache } from "react";
@@ -12,7 +12,6 @@ import { Page } from "@/app/type/types";
 import RelatedEpisodes from "../RelatedEpisodes";
 import AudioPlayer from "@/app/episodes/AudioPlayer";
 
-import Head from 'next/head';
 
 
 
@@ -58,11 +57,7 @@ const BlogPost = async ({ params: { slug } }: Props) => {
 
   return (
     <>
-    <Head>
-        <title>{post.title}</title>
-        <meta name="description" content={post.description} />
-        {/* Add other meta tags or customize as needed */}
-      </Head>
+    
     <ClientOnly>
 
     <Loader />
@@ -90,5 +85,69 @@ const BlogPost = async ({ params: { slug } }: Props) => {
 }
 
 export default BlogPost
+*/}
 
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
+import { cache } from "react";
+import SlugHero from "../SlugHero";
+import Container from "@/app/components/Container";
+import MediaLink from "@/app/components/MediaLink";
+import EpisodeSlugContainer from "./EpisodeSlugContainer";
+import Loader from "@/app/components/loader/Loader";
+import ClientOnly from "@/app/components/ClientOnly";
+import { Page } from "@/app/type/types";
+import RelatedEpisodes from "../RelatedEpisodes";
+import AudioPlayer from "@/app/episodes/AudioPlayer";
+import fetchMetadata from "@/lib/fetchMetadata";
 
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export const revalidate = 60;
+
+async function generateMetadata(slug: string) {
+  // Implement your logic to generate metadata here
+  // Example: fetching metadata from an API
+  const metadata = await fetchMetadata(slug);
+  return metadata;
+}
+
+const BlogPost = async ({ params: { slug } }: Props) => {
+  const metadata = await generateMetadata(slug); // Generate metadata before fetching the post data
+
+  const query = groq`*[_type=="episode" && slug.current == $slug][0]  {
+    ...,
+    categories[]->,
+    sponsors[]->,
+    relatedEpisodes[]->
+  }`;
+  const clientFetch = cache(client.fetch.bind(client));
+  const post = await clientFetch(query, { slug });
+
+  if (!post) return null;
+
+  return (
+    <>
+      <ClientOnly>
+        <Loader />
+        <div className="bg-gray">
+          <SlugHero />
+          <div className="max-w-[2250px] mx-auto xl:px-20 md:px-10 sm:px-2 px-0">
+            <EpisodeSlugContainer post={post} />
+          </div>
+          {post.relatedEpisodes && <RelatedEpisodes posts={post.relatedEpisodes} />}
+          <MediaLink />
+          <div className="fixed bottom-0 left-0 w-full">
+            {post.fileUrl && <AudioPlayer fileUrl={post.fileUrl} />}
+          </div>
+        </div>
+      </ClientOnly>
+    </>
+  );
+};
+
+export default BlogPost;
