3d17e7130be89cfda1dba5989b7d478d59a5a4c2
proofdig321 <134548873+proofdig321@users.noreply.github.com>
2023-08-05 07:18:44 +0000

    #edit

--- app/components/elements/ShareLinks.tsx
+++ app/components/elements/ShareLinks.tsx
@@ -1,4 +1,4 @@
-{/*
+
 import Link from "next/link";
 import { FaFacebook, FaLinkedin, FaTwitter, FaWhatsapp } from "react-icons/fa";
 import { Platforms } from "./socialLink";
@@ -59,77 +59,6 @@ const ShareLinks = ({ url }: { url: string }) => {
           </Link>
         );
       })}
-    </div>
-  );
-};
-
-export default ShareLinks;
-*/}
-
-import React from 'react';
-import Link from 'next/link';
-import { useRouter } from 'next/router'; // Import the useRouter hook
-import {
-  FaFacebook,
-  FaTwitter,
-  FaLinkedin,
-  FaWhatsapp,
-} from 'react-icons/fa';
-
-export enum Platforms {
-  Facebook = 'facebook',
-  Twitter = 'twitter',
-  Linkedin = 'linkedin',
-  Whatsapp = 'whatsapp',
-}
-
-const ShareLinks = () => {
-  const router = useRouter(); // Get the router instance
-  const currentUrl = encodeURIComponent(
-    process.env.NEXT_PUBLIC_SITE_URL + router.asPath // Construct the current URL
-  );
-
-  // To get full share URL
-  const getShareURL = (platform: Platforms) => {
-    switch (platform) {
-      case Platforms.Facebook:
-        return `https://www.facebook.com/sharer/sharer.php?u=${currentUrl}`;
-      case Platforms.Twitter:
-        return `https://twitter.com/intent/tweet?url=${currentUrl}`;
-      case Platforms.Linkedin:
-        return `https://www.linkedin.com/shareArticle?mini=true&url=${currentUrl}`;
-      case Platforms.Whatsapp:
-        return `https://wa.me/?text=${currentUrl}`;
-    }
-  };
-
-  // Render Icon
-  const renderIcons = (platform: Platforms) => {
-    switch (platform) {
-      case Platforms.Facebook:
-        return <FaFacebook size="20" />;
-      case Platforms.Twitter:
-        return <FaTwitter size="20" />;
-      case Platforms.Linkedin:
-        return <FaLinkedin size="20" />;
-      case Platforms.Whatsapp:
-        return <FaWhatsapp size="20" />;
-    }
-  };
-
-  return (
-    <div className="gap-5 flex md:flex-row items-center justify-between">
-      {Object.values(Platforms).map((platform: Platforms) => (
-        <Link
-          key={platform}
-          href={getShareURL(platform)}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="bg-neutral-100 w-full h-12 flex items-center justify-center rounded-md hover:bg-neutral-800 hover:text-neutral-50 duration-75 ease-out transition-colors"
-        >
-          {renderIcons(platform)}
-        </Link>
-      ))}
     </div>
   );
 };
