21f3377bcf208808651902fd8cedf25128224d44
anshu <lcb2021043@iiitl.ac.in>
2023-08-17 22:35:13 +0530

    added graph image in catagory page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,6 +9,7 @@ import ClientOnly from '../components/ClientOnly'
 export const metadata = {
   title: "About Us",
   description: "Unami Podcast is your gateway to a world of inspiration, creativity, and discovery. Join us on this immersive audio experience as we uncover the extraordinary stories, passions, and artistry that shape our lives. Tune in and let your senses come alive with Unami Podcast.",
+  
 };
 
 
@@ -26,5 +27,6 @@ const Aboutpage = () => {
 
   )
 }
+
 
 export default Aboutpage
--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -39,6 +39,26 @@ type Props = {
       return {
         title: tags.title,
         description: tags.description,
+        openGraph: {
+          title: tags.title,
+          description: tags.description,
+          url: process.env.SITE_URL + "/",
+          images: [
+            {
+              url: 'https://nextjs.org/og.png',
+              width: 800,
+              height: 600,
+            },
+            {
+              url: 'https://nextjs.org/og-alt.png',
+              width: 1800,
+              height: 1600,
+              alt: 'My custom alt',
+            },
+          ],
+          locale: 'en_US',
+          type: 'website',
+        },
       
       };
     } catch (error) {
